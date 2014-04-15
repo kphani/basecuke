b6c83620aad147bdadfc2c4c1be09efc2acf406d
kphani <pkaliginadi@gmail.com>
2014-04-16 00:05:08 +0100

    commited page objects

--- src/test/java/BaseClass.java
+++ src/test/java/BaseClass.java
@@ -11,28 +11,24 @@ import cucumber.api.java.en.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class BaseClass {
+public class  BaseClass {
     private  WebDriver driver;
+    LoginPage loginPage;
 
     public BaseClass() {
         this.driver = new FirefoxDriver();
-
-    }
-
-    @Given("^I am on the front page$")
-    public void i_am_on_the_front_page() {
-       //  driver.get("http://localhost:" + ServerHooks.PORT);
+        loginPage = new LoginPage(driver);
 
     }
 
     @Given("^I am on the Google page$")
     public void i_am_on_the_Google_page() {
-        driver.get("http://google.co.uk");
+        loginPage.navigateToAUrl("http://www.google.co.uk");
     }
 
     @Then("^I am on yahoo page$")
     public void I_am_on_yahoo_page() throws Throwable {
-        driver.get("http://yahoo.co.uk");
+        loginPage.navigateToAUrl("http://yahoo.co.uk");
     }
 }
 
